2103cadc510141c91401850681b49b1281f0b8e4
Bruno <bruno.schaatsbergen@park-now.com>
2021-05-29 14:02:06 +0200

    Playing around, trying to convert wkt to geojson

--- src/main.py
+++ src/main.py
@@ -1,13 +1,23 @@
 from fastkml import kml
+import geojson
+import shapely.wkt
 
+# Instantiate a KML Object
 k = kml.KML()
 
+# Read sample KML residing in data dir on root
 with open('C:\Github\PolyFiller\data\AT_Dornbirn.kml', 'rt', encoding="utf-8") as myfile:
     doc=myfile.read()
 
-
+# Assign the above data to the instantiated KML object
 k.from_string(doc)
 
+# Create a list of all the features in the KML
 features = list(k.features())
 
-print(features)
+primary_nested_features = list(features[0].features())
+
+for feature in primary_nested_features:
+    g1 = shapely.wkt.loads(feature.geometry.wkt)
+    g2 = geojson.Feature(geometry=g1, properties={})
+    print(g2.geometry.coordinates[0])
