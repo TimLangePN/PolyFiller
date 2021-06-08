2b0d0943879626f8e3947063e2e52b6878b8e6f3
TimLangePN <Tim.lange@park-now.com>
2021-06-09 01:02:47 +0200

    added csv logic

--- src/csv_writer.py
+++ src/csv_writer.py
@@ -1,20 +1,16 @@
 import csv
 import os
 
-
-schema = ['ZONE_CODE', 'LAT', 'LON', 'CITY', 'DISPLAY_STREETNAME',
-          'GOOGLE_STREETNAME', 'ZONE_STREET', 'ZONE_DESCRIPTION', 'Tariff_range', 'Unieke ID']
-
+schema = ['ZONE_CODE', 'LAT', 'LON', 'CITY', 'DISPLAY_STREETNAME', 'GOOGLE_STREETNAME', 'ZONE_STREET', 'ZONE_DESCRIPTION', 'Tariff_range', 'Unieke ID']
 
 def write_header(file_name):
     if os.path.exists(file_name):
         os.remove(file_name)
     with open(file_name, 'a', newline='') as file_name:
-        writer = csv.writer(file_name)
+        writer = csv.writer(file_name, delimiter=';', quoting=csv.QUOTE_NONE)
         writer.writerow([header for header in schema])
 
-
-def write_rows(file_name,coordinate, city_name, country_prefix, street_name):
+def write_rows(file_name,coordinate, city_name, country_prefix, street_name, tariff_range, unique_id):
     file_name = f'data\\{country_prefix}_{city_name}.csv'
 
     display_streetname = street_name
@@ -22,6 +18,5 @@ def write_rows(file_name,coordinate, city_name, country_prefix, street_name):
     zone_street = google_streetname
 
     with open(file_name, 'a', newline='') as file_name:
-        writer = csv.writer(file_name)
-        writer.writerow([coordinate.y, coordinate.x, city_name,
-                        display_streetname, google_streetname, zone_street])
+        writer = csv.writer(file_name, delimiter=';', quoting=csv.QUOTE_NONE)
+        writer.writerow([coordinate.y, coordinate.x, city_name, display_streetname, google_streetname, zone_street, tariff_range, unique_id])
--- src/main.py
+++ src/main.py
@@ -4,33 +4,58 @@ from reverse_geocoder import *
 from name_resolver import *
 from kml_parser import *
 from bootstrap import *
+from csv_helpers import *
 
 # Retrieve KML file from relative path
 kml_file = retrieve_kml()
 
-# Initialize a list that is used for storing N random generated points per feature
-coordinates_list = []
-
 # Init parse of the KML.
 features = parse_kml(kml_file)
 
-# Retrieve
-nested_features = list(features[0].features())
-
-# Get polygon and calculate random points inside polygon
-for feature in nested_features:
-    random_coordinates_list = generate_random_coordinates(5, feature.geometry)
-    for coordinates in random_coordinates_list:
-        coordinates_list.append(coordinates)
-
-# grabs the prefix from the reverse_geocoder and replaces countryname with a prefix
+# Extract country_prefix (e.g. 'FR') and city_name (e.g. 'Toulouse') from KML root
 country_prefix, city_name = get_country_prefix_and_city_name(features[0].name)
 
+# Concatenate country_prefix and city_name to file_name
 file_name = f'data\\{country_prefix}_{city_name}.csv'
 
 # Writes the header of the CSV
 write_header(file_name)
 
-for coordinates in coordinates_list:
-    street_name = resolve_street_name(str(coordinates.y), str(coordinates.x))
-    write_rows(file_name, coordinates, city_name, country_prefix, street_name)
+# Retrieve
+nested_features = list(features[0].features())
+
+# To be made dynamically so the client delegates the amount of random generated coordinates within a feature
+amount_of_random_generated_coordinates = 5
+
+# Initiliaze a counter that we increase per iteration, this is passed to the 'unique_id' variable
+counter = 0
+
+for feature in nested_features:
+
+    # Compute N random computed coordinates within the bounds of a feature.geometry object
+    random_coordinates_list = generate_random_coordinates(amount_of_random_generated_coordinates, feature.geometry)
+
+    # Retrieve the tariff_range (e.g. '1 - 1,99) from the styleUrl that's attached to a feature 
+    tariff_range = resolve_tariff_range(feature.styleUrl)
+
+    # Initialize a list that is used for storing N random generated points per feature
+    coordinates_list = []
+
+    # For every y and x in the list of randomly computed coordinates append it to the list of all coordinates 
+    for coordinates in random_coordinates_list:
+        coordinates_list.append(coordinates)
+
+    # For every y and x in the list of coordinates loop through this to persist records to the .csv
+    for coordinates in coordinates_list:
+
+        # Retrieve the street name for a computed random generated coordinate 
+        street_name = resolve_street_name(str(coordinates.y), str(coordinates.x))
+
+        # Calculate and construct the 'unique_id'
+        unique_id = f'{country_prefix}{city_name[0:3].upper()}{counter}'
+
+        # Persist row as record to file
+        write_rows(file_name, coordinates, city_name, country_prefix, street_name, tariff_range, unique_id)
+
+        # Increment counter per iteration
+        counter += 1
