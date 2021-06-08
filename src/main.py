0a4408dce727f8e294db69d58a90f2a1c46d5d55
TimLangePN <Tim.lange@park-now.com>
2021-06-08 18:18:24 +0200

    added city_name and city_prefix
    - still a work in progress

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 from inspect import Attribute
-import geojson
+import geojson                      # Are we going to use geojson ?
 from fastkml import kml
 from spatial_calculations import *
 from csv_writer import *
 from reverse_geocoder import *
+from name_resolver import *
 import json
 
 with open(".vscode\\load.json", "r") as jsonfile:
@@ -34,8 +35,16 @@ for feature in nested_features:
     for coordinates in random_coordinates_list:
         coordinates_list.append(coordinates)
 
-write_header('Freiburg')
+# grabs the prefix from the reverse_geocoder and replaces countryname with a prefix
+city_name = get_city_name(features)
+country_name = get_country_prefix(str(coordinates.y), str(coordinates.x))
+country_prefix = replace_country_name_to_prefix(country_name)
 
+# Writes the header of the CSV
+write_header(country_prefix, city_name)
+
+# Loops through the coordinates and writes them with the correct values
 for coordinates in coordinates_list:
     street_name = resolve_street_name(str(coordinates.y), str(coordinates.x))
-    write_rows(coordinates, 'Freiburg', street_name)
+
+    write_rows(coordinates, city_name, country_prefix, street_name)
